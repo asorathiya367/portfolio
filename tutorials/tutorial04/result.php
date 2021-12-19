3a810dbaff9fdf8f743b887ea2d049d83d936002
Akshay Patel <asorathiya367@rku.ac.in>
2021-12-19 11:27:38 +0530

    final

---
+++ tutorials/tutorial04/result.php
@@ -0,0 +1,12 @@
+<?php
+
+    if(isset($_POST['cal']))
+    {
+        $p = $_POST['p'];
+        $n = $_POST['n'];
+        $r = $_POST['r'];
+        $i = ($p*$n*$r)/100;
+        echo $i;
+    }
+
+?>
---
+++ tutorials/tutorial05/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <form action="result.php" method="POST">
+                <select name="month" value="select" id="month">
+                    
+                    <option value="1">January</option>
+                    <option value="2">February</option>
+                    <option value="3">March</option>
+                    <option value="4">April</option>
+                    <option value="5">May</option>
+                    <option value="6">June</option>
+                    <option value="7">July</option>
+                    <option value="8">Auguest</option>
+                    <option value="9">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                </select>
+                <button type="submit" class="btn btn-primary">
+                        Submit
+                    </button>
+                
+            </form>
+
+                
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
