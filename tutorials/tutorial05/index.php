30a42c7b022040d4664fbb87b6e41f9fcfc1d871
Akshay Patel <asorathiya367@rku.ac.in>
2021-12-19 17:38:44 +0530

    full nd final

--- tutorials/tutorial05/index.php
+++ tutorials/tutorial05/index.php
@@ -4,38 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Months</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Tutorial 5</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <form action="result.php" method="POST">
-                <select name="month" value="select" id="month" required>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <div class="card p-3">
+                    <div class="card-header text-center">
+                        <h4>Select Month</h4>
+                    </div>
+                    <form action="result.php" method="POST">
+                        <select class="form-select mt-1" name="month" value="select" id="month" required>
+                            <option value="">---Select Month---</option>
+                            <option value="1">January</option>
+                            <option value="2">February</option>
+                            <option value="3">March</option>
+                            <option value="4">April</option>
+                            <option value="5">May</option>
+                            <option value="6">June</option>
+                            <option value="7">July</option>
+                            <option value="8">Auguest</option>
+                            <option value="9">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+                        <div class="row justify-content-center">
+                             <div class="col-3">
+                                 <button name="cal" class="btn btn-primary mt-3">Calculate</button>
+                                </div>
+                            </div>
                     
-                    <option value="">---Select Month---</option>
-                    <option value="1">January</option>
-                    <option value="2">February</option>
-                    <option value="3">March</option>
-                    <option value="4">April</option>
-                    <option value="5">May</option>
-                    <option value="6">June</option>
-                    <option value="7">July</option>
-                    <option value="8">Auguest</option>
-                    <option value="9">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                </select>
-                <button type="submit" class="btn btn-primary">
-                        Submit
-                    </button>
-                
-            </form>
-
-                
-
+                </form>
+            </div>
+                </div>
             </div>
         </div>
     </div>
-</body>
+    </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
